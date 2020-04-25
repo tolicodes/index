097961c06cc2f7c42526c4e10b04befc8e65496e
Anatoliy Zaslavskiy <toli@tolicodes.com>
2020-04-25 14:25:29 +0800

    feat(infragen/generators/electron): added storybook

---
+++ projects/infragen/generators/electron/__tests__/index.ts
@@ -0,0 +1,5 @@
+import App from "../src/App";
+
+describe("@project/app", () => {
+  it("should do x", async () => {});
+});
---
+++ projects/infragen/generators/electron/src/stories/Home.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Home from '../Home';
+
+export default {
+  title: 'Home',
+  component: Home,
+};
+
+export const Default = () => (<Home />)
