32bbf062d87f68b31af0a712b1866220f529a751
Anatoliy Zaslavskiy <toli@tolicodes.com>
2020-04-25 17:54:26 +0800

    feat(storybook): added storybook instructions and files

---
+++ projects/infragen/generators/storybook.md
@@ -0,0 +1,7 @@
+# storybook
+
+```bash
+npx -p @storybook/cli sb init
+```
+
+Copy files from [storybook](./storybook/)
---
+++ projects/infragen/generators/storybook/stories/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Component from '../';
+
+export default {
+  title: 'Component',
+  component: Component,
+};
+
+export const Default = (<Component />)
